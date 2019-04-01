94eee8160ff7320f9d5cb219fc4b43306a2321a5
Cauê Aquino Nogueira <caue@FORLOGIC.LOCAL>
2019-04-01 14:58:05 -0300

    added function to render outside system views

---
+++ src/index.js
@@ -0,0 +1,23 @@
+// Javacript Document
+
+const containerData = document.getElementById('main-container')
+
+login = () => {
+    alert('login');
+    containerData.innerHTML=``;
+}
+
+createUser = () => {
+    alert('criar usuario');
+    containerData.innerHTML=``;
+}
+
+passwordRecovery = () => {
+    alert('recuperar senha');
+    containerData.innerHTML=``;
+}
+
+about = () => {
+    alert('sobre');
+    containerData.innerHTML=``;
+}
