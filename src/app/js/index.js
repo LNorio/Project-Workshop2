2d3eb0439e5e3f534048631bc156756a27644d34
Pedro Oliveira Bayma <pedro_bayma@outlook.com>
2019-04-02 23:19:07 -0300

    atualizacao input e buttons

--- src/app/js/index.js
+++ src/app/js/index.js
@@ -6,32 +6,3 @@ login = () => {
     alert('login');
     containerData.innerHTML=``;
 }
-
-createUser = () => {
-    alert('criar usuario');
-    containerData.innerHTML=``;
-}
-
-passwordRecovery = () => {
-    containerData.innerHTML=`<section id="main-container" class="section-content">
-                                <hgroup>
-                                    <h1>Inker</h1>
-                                    <h2>Cadastro</h2>
-                                </hgroup>
-
-                                <form action="">
-                                    <label for="email-input">E-mail:</label>
-                                    <input id="email-input" type="text" placeholder="Insira seu e-mail">
-
-                                </form>
-                                <section class="section-buttons">
-                                    <button onclick="login()">Recuperar</button>
-                                    <button onclick="">Voltar</button>
-                                </section>
-                            </section>`;
-}
-
-about = () => {
-    alert('sobre');
-    containerData.innerHTML=``;
-}
